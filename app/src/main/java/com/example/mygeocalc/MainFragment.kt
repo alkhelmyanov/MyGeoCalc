b603aa27ce3eb47da7bd994b89246aa3e67f115e
alexkhelmyanov <alkhelm@mail.ru>
2023-06-05 13:34:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mygeocalc/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.example.mygeocalc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container,false )
+    }
+}
